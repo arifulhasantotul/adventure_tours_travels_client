d44a6fc6db43f9f8f692b40d108ab34cf1b64874
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-10-29 13:27:43 +0600

    infinite

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,9 +1,31 @@
 import React from "react";
+import * as FaIcons from "react-icons/fa";
+import Rating from "react-rating";
 
-const Service = () => {
+const Service = (props) => {
+   const { name, rating, description, img } = props.service;
    return (
-      <div>
-         <h2>This is service</h2>
+      <div className="col">
+         <div className="box h-100">
+            <img src={img} alt="..." />
+            <div className="content">
+               <h3>
+                  {" "}
+                  <span>{name}</span>
+               </h3>
+               <p>{description.slice(0, 90)}...</p>
+               <div className="box_star">
+                  <Rating
+                     initialRating={rating}
+                     emptySymbol={<FaIcons.FaRegStar />}
+                     fullSymbol={<FaIcons.FaStar />}
+                     readonly
+                  />
+               </div>
+
+               <button className="btn_book">Book Now</button>
+            </div>
+         </div>
       </div>
    );
 };
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
 import "./Services.css";
 
 const Services = () => {
+   const [services, setServices] = useState([]);
+   useEffect(() => {
+      fetch("https://infinite-mountain-42809.herokuapp.com/services")
+         .then((res) => res.json())
+         .then((data) => setServices(data));
+   }, []);
    return (
-      <div>
+      <section className="container-fluid package_wrapper">
          <h1 class="heading">
             <span>s</span>
             <span>e</span>
@@ -14,7 +21,13 @@ const Services = () => {
             <span>e</span>
             <span>s</span>
          </h1>
-      </div>
+
+         <article className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            {services.map((item) => (
+               <Service key={item.key} service={item}></Service>
+            ))}
+         </article>
+      </section>
    );
 };
 
