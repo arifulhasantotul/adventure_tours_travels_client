f76e07e67281161e84a4c745aee2d2900d5622f8
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-10-31 14:25:34 +0600

    booking responsiv

--- src/components/Bookings/Booking.js
+++ src/components/Bookings/Booking.js
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import useAuth from "../../hooks/useAuth";
+import booking from "../../images/booking.svg";
 import OrderNav from "../../Pages/Orders/OrderNav";
+import "./Bookings.css";
 
 const Booking = () => {
    const { user } = useAuth();
@@ -10,6 +12,8 @@ const Booking = () => {
    const linkRef = useRef();
    const arrivalRef = useRef();
    const leaveRef = useRef();
+   const priceRef = useRef();
+   const itemRef = useRef();
 
    const handleBook = (e) => {
       e.preventDefault();
@@ -17,41 +21,63 @@ const Booking = () => {
    return (
       <section className="package_wrapper">
          <OrderNav></OrderNav>
-         <h1 class="heading">
+         <h1 class="heading book">
             <span>b</span>
             <span>o</span>
             <span>o</span>
             <span>k</span>
-            <span class="space"></span>
+            <span>i</span>
             <span>n</span>
-            <span>o</span>
-            <span>w</span>
+            <span>g</span>
          </h1>
-         <form className="form_login" onSubmit={handleBook}>
-            <input
-               type="text"
-               placeholder="Name"
-               ref={nameRef}
-               defaultValue={userName}
-               required
-            />
-            <input
-               type="text"
-               placeholder="Email"
-               defaultValue={userEmail}
-               ref={linkRef}
-               readOnly
-            />
-            <input
-               type="date"
-               placeholder="Arrivals"
-               ref={arrivalRef}
-               required
-            />
-            <input type="date" placeholder="Leaving" ref={leaveRef} required />
+         <article className="row">
+            <div className="col-12 col-lg-6">
+               <img src={booking} className="img-fluid" alt="" />
+            </div>
+            <form className="form_login col-12 col-lg-6" onSubmit={handleBook}>
+               <input
+                  type="text"
+                  placeholder="Name"
+                  ref={nameRef}
+                  defaultValue={userName}
+                  required
+               />
+               <input
+                  type="text"
+                  placeholder="Email"
+                  defaultValue={userEmail}
+                  ref={linkRef}
+                  readOnly
+                  required
+               />
+               <input
+                  type="date"
+                  placeholder="Arrivals"
+                  ref={arrivalRef}
+                  required
+               />
+               <input
+                  type="date"
+                  placeholder="Leaving"
+                  ref={leaveRef}
+                  required
+               />
+               <input
+                  type="number"
+                  placeholder="Leaving"
+                  ref={itemRef}
+                  required
+               />
+               <input
+                  type="text"
+                  placeholder="Leaving"
+                  ref={priceRef}
+                  required
+               />
 
-            <input type="submit" value="Book Now" />
-         </form>
+               <input type="submit" value="Book Now" />
+            </form>
+         </article>
       </section>
    );
 };
